900b598357f866046e74613ccc7c9ca0b720eb4c
Saint George <st.george.github@gmail.com>
2018-01-20 18:52:43 +0200

    ...

--- src/scope-automation.py
+++ src/scope-automation.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
+import subprocess
 import sys
 #import select
 import readchar
@@ -13,22 +14,57 @@ import subprocess
 import math
 import psutil
 from serial.serialutil import SerialException 
+import io
+import logging
+import gphoto2 as gp
+import imageio
+import scipy.optimize
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
 
 ARDUINO_PORT = "/dev/tty.usbmodem1421"
 ARDUINO_SPEED = "115200"
 
 CAMERA = None
-CAMERA_PORT = None
 DELTA = 40
 REFERENCE_FFT_RESULT = None
 REFERENCE_Z_POSITION = None
+MAX_DELTA_TO_REFERENCE_Z_POSITION = 500
 Z_POSITION = 0
-GPHOTO2_BIN = "/usr/local/bin/gphoto2"
+AUTOFOCUS_BOUND = 50
 
-CORRELATIONS = {}
-CORRELATIONS_RESETED = None
-CORRELATIONS_RESET_DELTA = 60 * 15 # reset correlations after 15 minutes
-MAX_FOCUS_DELTA = 100
+class ImageWebSocket(tornado.websocket.WebSocketHandler):
+    clients = set()
+
+    def check_origin(self, origin):
+        # Allow access from every origin
+        return True
+
+    def open(self):
+        ImageWebSocket.clients.add(self)
+        print("WebSocket opened from: " + self.request.remote_ip)
+
+    def on_message(self, message):
+        jpeg_bytes = get_preview_image()
+        self.write_message(jpeg_bytes, binary=True)
+
+    def on_close(self):
+        ImageWebSocket.clients.remove(self)
+        print("WebSocket closed from: " + self.request.remote_ip)
+
+script_path = os.path.dirname(os.path.realpath(__file__))
+static_path = script_path + '/static/'
+
+app = tornado.web.Application([
+        (r"/websocket", ImageWebSocket),
+        (r"/(.*)", tornado.web.StaticFileHandler, {'path': static_path, 'default_filename': 'index.html'}),
+    ])
+app.listen(args.port)
+
+print("Starting server: http://localhost:" + str(args.port) + "/")
+
+tornado.ioloop.IOLoop.current().start()
 
 class Error(Exception):
     """Base class for exceptions in this module."""
@@ -65,8 +101,8 @@ def kill_ptpcamera():
                                                                                       pid=proc.pid))
       proc.kill()
 
-def sample_and_fft(fname):
-  img = scipy.ndimage.imread(fname, True)
+def sample_and_fft(image_data):
+  img = imageio.imread(image_data, "JPEG-PIL")
   res = None
   for i in xrange(0, img.shape[0], DELTA):
     vec = np.absolute(numpy.fft.fft(img[:][i].flatten()))**2
@@ -76,41 +112,39 @@ def sample_and_fft(fname):
       res = np.concatenate([res, vec])
   return res
 
-def capture_image_filename():
-  return "data/image-{count}.%C".format(count=math.trunc(time() * 1000))
+def image_filename():
+  return "data/image-{count}.jpg".format(count=math.trunc(time() * 1000))
 
-def reset_correlations(force=False):
-  global CORRELATIONS, CORRELATIONS_RESETED
-  if force or CORRELATIONS_RESETED is None or time() - CORRELATIONS_RESETED > CORRELATIONS_RESET_DELTA:
-    log("Correlations reseted")
-    CORRELATIONS = {}
-    CORRELATIONS_RESETED = time()
+def get_preview_image():
+  camera_file = gp.check_result(gp.gp_camera_capture_preview(CAMERA))
+  file_data = gp.check_result(gp.gp_file_get_data_and_size(camera_file))
+  buf = io.BytesIO(file_data)
+  buf.seek(0)
+  return buf
 
-def capture_image(use_as_reference=False):
-  global REFERENCE_FFT_RESULT, REFERENCE_Z_POSITION, Z_POSITION, CORRELATIONS, CORRELATIONS_RESETED
-  fname = capture_image_filename()
-  out = subprocess.check_output([ 
-    GPHOTO2_BIN,
-    "--port", CAMERA_PORT, 
-    "--capture-image-and-download", 
-    "--quiet",
-    "--filename", fname])
+def get_store_and_show_image():
+  file_path = gp.check_result(gp.gp_camera_capture(CAMERA, gp.GP_CAPTURE_IMAGE))
+  log('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
+  target = image_filename()
+  print 'Copying image to {target}'.format(target=target)
+  camera_file = gp.check_result(gp.gp_camera_file_get(CAMERA, file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL))
+  gp.check_result(gp.gp_file_save(camera_file, target))
+  subprocess.call(['open', target])
+  return target
 
-  jpeg_fname = fname.replace("%C", "jpg")
-  fft_result = sample_and_fft(jpeg_fname)
+def current_position_take_preview_image_and_set_as_reference():
+  global REFERENCE_FFT_RESULT, REFERENCE_Z_POSITION
+  image = get_preview_image()
+  REFERENCE_FFT_RESULT = sample_and_fft(image)
+  REFERENCE_Z_POSITION = Z_POSITION
+  print "Using current position {pos} as reference".format(pos=Z_POSITION)
 
-  if use_as_reference or REFERENCE_FFT_RESULT is None:
-    log("Captured image {fname}, used as reference image".format(fname=jpeg_fname))
-    REFERENCE_FFT_RESULT = fft_result
-    REFERENCE_Z_POSITION = Z_POSITION
-    reset_correlations(force=True)
-  else:
-    reset_correlations()
-    corr = np.corrcoef(REFERENCE_FFT_RESULT, fft_result)[1,0]
-    CORRELATIONS[Z_POSITION] = corr
-    log("Captured image {fname}, correlation {corr}".format(fname=jpeg_fname, corr=corr))
-
-  return jpeg_fname
+def current_position_take_image_and_get_correlation_with_reference():
+  image = get_preview_image()
+  fft_result = sample_and_fft(image)
+  corr = np.corrcoef(REFERENCE_FFT_RESULT, fft_result)[1,0]
+  log("Captured image position {pos} correlation {corr}".format(pos=Z_POSITION, corr=corr))
+  return corr
 
 def connect_arduino(ser):
   data = serial_readline(ser)
@@ -118,40 +152,44 @@ def connect_arduino(ser):
     raise SystemError("Expected HELLO, but got {data}".format(data=data))
   
 def connect_camera():
-  global CAMERA, CAMERA_PORT
-  if not os.path.exists(GPHOTO2_BIN):
-    raise SystemError("gphoto2 binary does not exist at {path}".format(path=GPHOTO2_BIN))
+  global CAMERA
 
-  p = subprocess.Popen([GPHOTO2_BIN, "--auto-detect"], stdout=subprocess.PIPE)
-  out, err = p.communicate()
-  lines = out.splitlines()
+  logging.basicConfig(format='%(levelname)s: %(name)s: %(message)s', level=logging.WARNING)
+  gp.check_result(gp.use_python_logging())
+  CAMERA = gp.check_result(gp.gp_camera_new())
+  gp.check_result(gp.gp_camera_init(CAMERA))
 
-  if len(lines) == 2:
-    raise PeripheralStatusError("No camera found")
-
-  if 'Model' not in lines[0]:
-    raise SystemError("Unexpected output from gphoto2 --auto-detect")
-
-  if '----' not in lines[1]:
-    raise SystemError("Unexpected output from gphoto2 --auto-detect")
-
-  if len(lines) != 3:
-    raise PeripheralStatusError("Only exactly one camera is now supported")
-
-  CAMERA = lines[2][0:30].strip()
-  CAMERA_PORT = lines[2][31:]
-
-  log("Found camera {camera} on port {port}".format(camera=CAMERA, port=CAMERA_PORT))
+  # required configuration will depend on camera type!
+  print "Checking camera config"
+  # get configuration tree
+  config = gp.check_result(gp.gp_camera_get_config(CAMERA))
+  # find the image format config item
+  OK, image_format = gp.gp_widget_get_child_by_name(config, 'imageformat')
+  if OK >= gp.GP_OK:
+      # get current setting
+      value = gp.check_result(gp.gp_widget_get_value(image_format))
+      # make sure it's not raw
+      if 'raw' in value.lower():
+          raise PeripheralStatusError('Camera is setup to record raw, but we need previs, and preview does not work with raw images')
+  # find the capture size class config item
+  # need to set this on my Canon 350d to get preview to work at all
+  OK, capture_size_class = gp.gp_widget_get_child_by_name( config, 'capturesizeclass')
+  if OK >= gp.GP_OK:
+      # set value
+      value = gp.check_result(gp.gp_widget_get_choice(capture_size_class, 2))
+      gp.check_result(gp.gp_widget_set_value(capture_size_class, value))
+      # set config
+      gp.check_result(gp.gp_camera_set_config(CAMERA, config))
 
 def ready():
-  log("Ready, q = quit, a = -1, z = +1, t = take image, r = take image and use as reference, f = focus to reference image")
+  log("Ready, q = quit, a = -1, z = +1, t = take and show image, r = use current position as reference, f = find position with highest correlation with reference")
 
 def setup():
   try:
     ser = serial.Serial(ARDUINO_PORT, baudrate=ARDUINO_SPEED)
   except SerialException as err:
     raise PeripheralStatusError("Could not connect to Arduino: " + err.strerror)
-  
+
   return ser
 
 def getchar():
@@ -179,29 +217,31 @@ def serial_readline(ser):
 
 def move_z(ser, value):
   global Z_POSITION
+  if abs(Z_POSITION - REFENCE_Z_POSITION) > MAX_DELTA_TO_REFERENCE_Z_POSITION:
+    raise PeripheralStatusError("Reached maximum delta to reference z position")
   Z_POSITION += value
   serial_writeline(ser, value)
   log("Z: {z}".format(z=Z_POSITION))
 
-def focus_get_correlation(ser, pos):
-  if pos in CORRELATIONS:
-    return CORRELATIONS[pos]
-  else:
-    if math.abs(REFERENCE_Z_POSITION - pos) > MAX_FOCUS_DELTA:
-      raise PeripheralStatusError("We are too far away from reference z position, maximum allowed is {max_focus_delta}".format(max_focus_delta=MAX_FOCUS_DELTA))
-    move_z(ser, Z_POSITION - pos)
-    capture_image()
-    return CORRELATIONS[pos]
-
-def focus(ser):
-  original_position = Z_POSITION
-  for x in [ -10, -5, 0, 5, 10 ]:
-    focus_get_correlation(ser, original_position + x)
-  z = np.polyfit(CORRELATIONS.keys(), CORRELATIONS.values(), 2)
+def find_position_with_lowest_correlation_with_reference(ser, level=1):
+  if level > 5:
+    raise PeripheralStatusError("Reached maximum limit for autofocus")
+  move_z(ser, -1 * AUTOFOCUS_BOUND)
+  xa = xrange(-1 * AUTOFOCUS_BOUND, AUTOFOCUS_BOUND, 5)
+  ya = []
+  for i in xa:
+    move_z(ser, 2)
+    corr = current_position_take_image_and_get_correlation_with_reference()
+    log("Index %d has correlation %r)" % (i, corr))
+    ya.append(corr)
+  z = np.polyfit(xa, ya, 2)
   p = np.poly1d(z)
-  scipy.optimize.minimize_scalar(p)
-  pass
-  
+  result = scipy.optimize.minimize_scalar(-1 * p, method='bounded', bounds=[-1 * AUTOFOCUS_BOUND, AUTOFOCUS_BOUND])
+  new_pos = round(result.x)
+  move_z(ser, -1 * AUTOFOCUS_BOUND + new_pos)
+  if abs(new_pos) + 3 > AUTOFOCUS_BOUND:
+    find_position_with_lowest_correlation_with_reference(ser, level + 1)
+
 def main():
   ser = setup()
 
@@ -218,11 +258,11 @@ def main():
     elif ch == 'z':
       move_z(ser, 1)
     elif ch == 't':
-      capture_image()
+      get_and_store_image()
     elif ch == 'r':
-      capture_image(use_as_reference=True)
+      current_position_take_preview_image_and_set_as_reference()
     elif ch == 'f':
-      focus()
+      find_position_with_lowest_correlation_with_reference(ser)
     elif ch == 'q':
       log("Exiting");
       sys.exit(0)
@@ -230,6 +270,9 @@ def main():
 if __name__ == "__main__":
   try:
     main()
+    IOLoop.add_handler(sys.stdin, 
+
+    sys.exit(0)
   except PeripheralStatusError as err:
     print err.message
     sys.exit(1)
